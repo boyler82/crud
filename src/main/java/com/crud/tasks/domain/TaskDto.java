c5e0c0cea9646cc40041ef84f1031220d1510f2c
tron <tomasz26bojko@gmail.com>
2025-02-20 11:29:42 +0100

    usuwanie zadania

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -13,4 +13,5 @@ public class TaskDto {
     private Long id;
     private String title;
     private String content;
+
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,11 +1,11 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,16 +19,15 @@ public class DbService {
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
-    public Task getTaskById(final Long taskId) {
-        return repository.findById(taskId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Task with id not found"));
+    public Task saveTask(final Task task) {
+        return repository.save(task);
     }
 
-//    public Optional<Task> getTask(final Long taskId) {
-//        return repository.findById(taskId);
-//    }
-//
-//    public Task saveTask(final Task task) {
-//        return repository.save(task);
-//    }
+    public void deleteTask(final Long taskId) {
+         repository.deleteById(taskId);
+    }
+
+    public Task getTask(final Long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
+    }
 }
