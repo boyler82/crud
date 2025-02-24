f334b71640a9f70bae9762cc3d86ca47600d6ee8
tron <tomasz26bojko@gmail.com>
2025-02-24 21:08:01 +0100

    ulozenie kodu

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,19 @@
+package com.crud.tasks.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor lub samo @Data
+@Data
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+
+
+
+}
